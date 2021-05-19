eee1174f08ae36c58b73658a6be4a107795c7ea4
chul0721 <chul070721@naver.com>
2021-05-19 16:39:27 +0900

    style: Prettier

--- src/events/onMessage.ts
+++ src/events/onMessage.ts
@@ -5,11 +5,11 @@ import Query from '../struct/Query'
 const onMessage = (client: Client, message: Discord.Message) => {
   const { prefix } = client.config
   const { author, content } = message
-  
+
   if (!message.guild) return
   if (author.bot) return
 
-  if(client.user){
+  if (client.user) {
     if (message.mentions.has(client.user.id)) {
       if (message.content.includes('@here') || message.content.includes('@everyone')) return
       return message.channel.send(';')
